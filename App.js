9bce9193229575a13287915f1c1f153b3bb4209e
danacr <dan@acristinii.com>
2019-12-07 21:32:08 +0100

    First working version

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { Grid, Col, Row } from "react-native-easy-grid";
+import { ShakeEventExpo } from "./ShakeEventExpo";
+import Constants from "expo-constants";
+
+export default class App extends Component {
+  name = Constants.deviceName;
+  id = Constants.installationId;
+  async componentWillMount() {
+    ShakeEventExpo.addListener(() => {
+      fetch("https://k8s-disrupter.scapp.io/", {
+        method: "POST",
+        body: JSON.stringify({ Name: this.name, ID: this.id })
+      }).catch();
+    });
+  }
+
+  render() {
+    return (
+      <Grid style={{ backgroundColor: "black" }}>
+        <Row style={{ alignItems: "center" }} size={1}>
+          <Col style={{ alignItems: "center" }}>
+            <Text style={{ color: "#fff", fontSize: 32 }}>Hi {this.name}</Text>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
